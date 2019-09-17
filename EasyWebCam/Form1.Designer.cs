39d6880dbf391242b710294880419efcafb35057
Leo Profile <leoprofissional@live.com>
2019-09-17 07:42:45 -0300

    first commit

---
+++ EasyWebCam/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace EasyWebCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbWeb = new System.Windows.Forms.PictureBox();
+            this.btnLigarDesligar = new System.Windows.Forms.Button();
+            this.btnCapturar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWeb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbWeb
+            // 
+            this.pbWeb.Location = new System.Drawing.Point(13, 13);
+            this.pbWeb.Name = "pbWeb";
+            this.pbWeb.Size = new System.Drawing.Size(514, 244);
+            this.pbWeb.TabIndex = 0;
+            this.pbWeb.TabStop = false;
+            // 
+            // btnLigarDesligar
+            // 
+            this.btnLigarDesligar.Location = new System.Drawing.Point(13, 277);
+            this.btnLigarDesligar.Name = "btnLigarDesligar";
+            this.btnLigarDesligar.Size = new System.Drawing.Size(514, 23);
+            this.btnLigarDesligar.TabIndex = 1;
+            this.btnLigarDesligar.Text = "Ligar/Desligar";
+            this.btnLigarDesligar.UseVisualStyleBackColor = true;
+            this.btnLigarDesligar.Click += new System.EventHandler(this.btnLigarDesligar_Click);
+            // 
+            // btnCapturar
+            // 
+            this.btnCapturar.Location = new System.Drawing.Point(13, 317);
+            this.btnCapturar.Name = "btnCapturar";
+            this.btnCapturar.Size = new System.Drawing.Size(514, 23);
+            this.btnCapturar.TabIndex = 2;
+            this.btnCapturar.Text = "Capturar";
+            this.btnCapturar.UseVisualStyleBackColor = true;
+            this.btnCapturar.Click += new System.EventHandler(this.btnCapturar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 352);
+            this.Controls.Add(this.btnCapturar);
+            this.Controls.Add(this.btnLigarDesligar);
+            this.Controls.Add(this.pbWeb);
+            this.Name = "Form1";
+            this.Text = "EasyWebCam";
+            ((System.ComponentModel.ISupportInitialize)(this.pbWeb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbWeb;
+        private System.Windows.Forms.Button btnLigarDesligar;
+        private System.Windows.Forms.Button btnCapturar;
+    }
+}
+
---
+++ EasyWebCam/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+
+namespace EasyWebCam
+{
+    public partial class Form1 : Form
+    {
+        private VideoCaptureDevice videoSource;
+        public Form1()
+        {
+            InitializeComponent();
+
+            var videoSources = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoSources != null && videoSources.Count > 0)
+            {
+                videoSource = new VideoCaptureDevice(videoSources[0].MonikerString);
+                videoSource.NewFrame += VideoSource_NewFrame;
+            }
+        }
+
+        private void VideoSource_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            if (pbWeb.Image != null)
+            {
+                pbWeb.Image.Dispose();
+            }
+            pbWeb.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void btnLigarDesligar_Click(object sender, EventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.Stop();
+                pbWeb.Image = null;
+            }
+            else
+            {
+                videoSource.Start();
+            }
+        }
+
+        private void btnCapturar_Click(object sender, EventArgs e)
+        {
+            if (pbWeb.Image != null)
+            {
+                try
+                {
+                    videoSource.NewFrame -= VideoSource_NewFrame;
+
+                    using (var dialog = new SaveFileDialog())
+                    {
+                        dialog.DefaultExt = "png";
+                        dialog.AddExtension = true;
+
+                        if (dialog.ShowDialog() == DialogResult.OK)
+                        {
+                            pbWeb.Image.Save(dialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                        }
+                    }
+                }
+                finally
+                {
+                    videoSource.NewFrame += VideoSource_NewFrame;
+                }
+                
+            }
+
+           
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.Stop();
+            }
+            base.OnFormClosing(e);
+        }
+    }
+}
